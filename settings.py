d247188159ca6ed365357e0a41b521f4378a84ce
b0073d <tim@b0073d.com>
2014-04-18 21:47:25 +1000

    Added screenshot

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 __author__ = 'Tim Suess'
 
-size = width, height = 150, 150
+size = width, height = 200, 200
 origin = [5, 5]
-target = [127, 148]
-obstacle_ratio = 4
+target = [195, 195]
+obstacle_ratio = 3
 # pygame display colours
 black = 0, 0, 0
 red = 255, 100, 100
